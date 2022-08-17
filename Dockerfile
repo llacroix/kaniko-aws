5fcc6c22d300820d956c85fb153f6a6526b496c5
Loïc Faure-Lacroix <lamerstar@gmail.com>
2022-08-17 14:32:35 -0400

    Init Repository

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+from gcr.io/kaniko-project/executor:latest as kaniko
+from ubuntu:22.04
+
+# Copy kaniko data to this image
+copy --from=kaniko /kaniko /kaniko                                                                                         
+copy script.py /kaniko/app/script.py
+copy requirements.txt /kaniko/app/requirements.txt
+
+# Prepare application
+RUN apt-get update && \
+    apt-get install -y ca-certificates python3 python3-pip && \
+    /usr/bin/python3 -m pip install -t /kaniko/app -r /kaniko/app/requirements.txt && \
+    cp -r /etc/ssl/certs/* /kaniko/ssl/certs && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /root/.cache
+
+# Prepare environment
+ENV PATH=/usr/local/bin:/kaniko:/usr/bin
+ENV HOME=/root
+ENV USER=root
+ENV SSL_CERT_DIR=/kaniko/ssl/certs
+ENV DOCKER_CONFIG=/kaniko/.docker/
+ENV DOCKER_CREDENTIAL_GCR_CONFIG=/kaniko/.config/gcloud/docker_credential_gcr_config.json
+
+# Start application
+cmd ["/usr/bin/python3", "/kaniko/app/script.py"]
---
+++ script.py
@@ -0,0 +1,215 @@
+# Import everything that's necessary before cleaning up filesystem
+import click
+import os
+import jedi # noqa
+import json
+import boto3
+import requests
+import encodings.idna
+import encodings.cp437 # noqa
+from urllib.parse import urlparse
+from os.path import islink
+import sys
+from pathlib import Path
+from shutil import rmtree
+from ptpython import embed
+from ptpython.style import get_all_code_styles, get_all_ui_styles
+from pygments.styles import get_style_by_name # noqa
+from prompt_toolkit.key_binding.defaults import load_key_bindings
+from prompt_toolkit.input.defaults import create_input
+from subprocess import run
+from concurrent.futures import thread # noqa
+import zipfile
+import logging
+
+_logger = logging.getLogger(__name__)
+
+s3 = boto3.client('s3')
+ecr = boto3.client('ecr')
+
+CONTEXT_DIR = "/kaniko/build"
+
+
+def preload_application():
+    get_all_ui_styles()
+    get_all_code_styles()
+    load_key_bindings()
+    create_input()
+
+
+def clean_folder(path):
+
+    if islink(path):
+        path.unlink()
+    elif path.is_file():
+        path.unlink()
+    elif path.is_dir():
+        rmtree(path)
+
+    _logger.debug("Removing %s", path)
+
+
+def clean_filesystem():
+    safe_paths = [
+        Path(x)
+        for x in [
+            '/dev',
+            '/etc',
+            '/proc',
+            '/sys',
+            '/tmp',
+            '/var',
+            '/kaniko',
+            '/project',
+            '/project2'
+        ]
+    ]
+
+    empty_paths = [
+        Path('/tmp'),
+        Path('/etc'),
+        Path('/var')
+    ]
+
+    for path in Path('/').iterdir():
+        if path in safe_paths:
+            continue
+        clean_folder(path)
+
+    for path in empty_paths:
+        for p in path.iterdir():
+            try:
+                clean_folder(p)
+            except Exception:
+                pass
+
+
+def get_dockerfile_path(dockerfile):
+    parsed = urlparse(dockerfile)
+    if not parsed.netloc:
+        return Path(dockerfile)
+
+    path = Path('/kaniko/Dockerfile')
+
+    if parsed.scheme == 's3':
+        s3.download_file(
+            parsed.netloc,
+            parsed.path,
+            str(path)
+        )
+        return path
+    elif parsed.scheme in ['http', 'https']:
+        req = requests.get(dockerfile)
+        with path.open('wb') as fout:
+            fout.write(req.content)
+        return path
+
+
+def build_image(dockerfile="/kaniko/Dockerfile", target=None):
+    params = [
+        '/kaniko/executor',
+        '--context', CONTEXT_DIR,
+        '-f', str(dockerfile),
+        '--cleanup'
+    ]
+
+    if target is None:
+        params.append('--no-push')
+    else:
+        params += [
+            '--destination', target
+        ]
+
+    return run(params)
+
+
+def docker_login():
+    auths = ecr.get_authorization_token()
+
+    token = auths['authorizationData'][0]
+
+    url = urlparse(token['proxyEndpoint'])
+
+    data = {
+        'auths': {
+            url.netloc: {
+                "auth": token['authorizationToken']
+            }
+        }
+    }
+
+    docker_file = Path('/kaniko/.docker/config.json')
+    with docker_file.open('w') as fout:
+        fout.write(json.dumps(data))
+
+
+def create_build_context(bucket, object_name):
+    build_path = Path(CONTEXT_DIR)
+
+    if build_path.exists():
+        rmtree(build_path)
+
+    build_path.mkdir()
+
+    if bucket and object_name:
+        s3.download_file(bucket, object_name, '/tmp/build.zip')
+
+        with zipfile.ZipFile('/tmp/build.zip', 'r') as zip_ref:
+            zip_ref.extractall(str(build_path))
+
+        Path('/tmp/build.zip').unlink()
+
+
+def automated_build(dockerfile_path, bucket, data_object, repo):
+    create_build_context(bucket, data_object)
+    docker_login()
+    build_image(dockerfile_path, target=repo)
+    return 0
+
+
+def interactive_main():
+    return embed(globals(), locals())
+
+
+@click.command()
+@click.option(
+    '--interactive',
+    is_flag=True,
+    default=False
+)
+@click.option(
+    '--bucket',
+    help="S3 Bucket for data",
+)
+@click.option(
+    '--context-object',
+    help="S3 Object containing context data",
+)
+@click.option(
+    '--repo',
+    help="Destination Repository"
+)
+@click.argument(
+    'dockerfile'
+)
+def main(interactive, bucket, context_object, repo, dockerfile):
+    if not bucket:
+        bucket = os.environ.get('S3_BUCKET')
+
+    if not context_object:
+        context_object = os.environ.get('S3_OBJECT')
+
+    if not repo:
+        repo = os.environ.get('ECR_REPO')
+
+    preload_application()
+    clean_filesystem()
+
+    dockerfile_path = get_dockerfile_path(dockerfile)
+
+    if interactive:
+        ret = interactive_main(bucket, context_object, repo)
+    else:
+        ret = automated_build(dockerfile_path, bucket, context_object, repo)
+
+    sys.exit(ret)
